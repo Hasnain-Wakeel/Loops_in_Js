6c4cecd8c28a9b03358ee9108c40e999e9cf3fbf
Hasnain-Wakeel <wakeelanjum112@gmail.com>
2025-09-25 17:53:39 +0500

    Basics of Loops

---
+++ loops.js
@@ -0,0 +1,48 @@
+// What is a for loop ??
+
+// A for loop is used when you know how many times you want to repeat a block of code.
+// It has three parts inside the parentheses .
+
+// Necessary Things In Loop are :
+//       (i)            (c)            (u)
+//   initialization ; condition ;     updation
+
+// General Syntax Of For Loop :
+
+// for (icu) {
+//     // Code To Run
+// }
+
+// for(let a = 1 ; a <= 7 ; a++){
+//     console.log("Value of a is " + a)         // Here + joins the string "Value of a is " with the variable a.
+//                                                         // The result becomes a single string.
+//     console.log("Value of a is " , a)        // Comma (separate operator)
+//     // console.log(typeof(a))
+// }
+
+// let students = ["ali", "anas", "saad", "zain"];
+// for (i = 0; i < students.length; i++) {
+//     console.log(i)
+//   console.log(students[i]);
+// }
+
+// // console.log(students[i]);             // This returns undefined bcz the value of i is 4 and 
+//                                                    // the array has only 4 elements.
+//   console.log(`Value of i outside the loop is ${i}`)
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
